306da8574b7cff4e09ba91c6f33b0493ecf2dcca
Victor Lap <victorlap@outlook.com>
2022-12-07 10:36:00 +0100

    Day 07

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod days;
 mod etc;
 
-use days::{day01, day02, day03, day04, day05, day06};
+use days::{day01, day02, day03, day04, day05, day06, day07};
 use etc::solution::Solution;
 use std::env;
 use std::time::Instant;
@@ -50,7 +50,7 @@ fn get_day_solver(day: u8) -> fn() -> SolutionPair {
         4 => day04::solve,
         5 => day05::solve,
         6 => day06::solve,
-        // 7 => day07::solve,
+        7 => day07::solve,
         // 8 => day08::solve,
         // 9 => day09::solve,
         // 10 => day10::solve,
