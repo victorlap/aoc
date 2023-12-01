9793338ff619702f4561fd2d0128876245527471
Victor Lap <victorlap@outlook.com>
2023-12-01 12:19:09 +0100

    Advent Of Code 2023

---
+++ src/days/day16.rs
@@ -0,0 +1,7 @@
+use crate::{Solution, SolutionPair};
+
+///////////////////////////////////////////////////////////////////////////////
+
+pub fn solve() -> SolutionPair {
+    (Solution::U64(0), Solution::U64(0))
+}
