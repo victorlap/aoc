8ef3258f535225859219f3e439630aafcb145534
Victor Lap <victorlap@outlook.com>
2023-12-04 08:56:43 +0100

    Update boilerplate

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,7 +1,21 @@
 use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    (Solution::U64(0), Solution::U64(0))
+    let input: String = read_to_string("src/input/day03.txt").unwrap();
+    let parsed: Vec<String> = input.lines().map(|line| line.parse().unwrap()).collect();
+
+    (Solution::U64(sol1(&parsed)), Solution::U64(sol2(&parsed)))
 }
+
+fn sol1(input: &Vec<String>) -> u64 {
+    0
+}
+
+fn sol2(input: &Vec<String>) -> u64 {
+    0
+}
+
+///////////////////////////////////////////////////////////////////////////////
