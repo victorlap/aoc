ddd84fc9f10661a8e3908f3f92129a4a096e0376
Victor Lap <victorlap@outlook.com>
2023-12-06 08:53:40 +0100

    Day 06

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,21 +1,41 @@
+use std::task::Wake;
+
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    let input: String = read_to_string("src/input/day03.txt").unwrap();
-    let parsed: Vec<String> = input.lines().map(|line| line.parse().unwrap()).collect();
+    // Time:      7  15   30
+    // Distance:  9  40  200
+    let _testing = vec![(7, 9), (15, 40), (30, 200)];
 
-    (Solution::U64(sol1(&parsed)), Solution::U64(sol2(&parsed)))
+    // Time:        54     70     82     75
+    // Distance:   239   1142   1295   1253
+    let input = vec![(54, 239), (70, 1142), (82, 1295), (75, 1253)];
+
+    (Solution::U64(sol1(&input)), Solution::U64(sol2()))
 }
 
-fn sol1(_input: &Vec<String>) -> u64 {
-    0
+fn sol1(input: &Vec<(u64, u64)>) -> u64 {
+    input
+        .into_iter()
+        .map(|i| determine_ways(i.0, i.1))
+        .fold(1, |acc, i| acc * i)
 }
 
-fn sol2(_input: &Vec<String>) -> u64 {
-    0
+fn sol2() -> u64 {
+    // determine_ways(71530, 940200)
+    determine_ways(54708275, 239114212951253)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+
+fn determine_ways(time: u64, distance: u64) -> u64 {
+    let mut valid = 0;
+    for i in 0..time {
+        if (time - i) * i > distance {
+            valid += 1;
+        }
+    }
+    valid
+}
