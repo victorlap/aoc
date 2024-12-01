4328472394ee199caab59a232544035a635019d4
Victor <victorlap@outlook.com>
2024-12-01 19:59:19 +0000

    Day 1 part 1

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,9 +1,31 @@
-import { parseLines, readInput } from 'io'
+import { parseLines, readInput } from 'io';
 
 const input = await readInput('day-01')
 
 export const part1 = () => {
-  const lines = parseLines(input)
-  // your code goes here
-  return lines.length
+  const lists = getSortedLists();
+  let answer = 0;
+
+  for (let i = 0; i < lists[0].length; i++) {
+    answer += Math.abs(lists[0][i] - lists[1][i]);
+  }
+ 
+  return answer
+}
+
+const getSortedLists =() => {
+  const lines = parseLines(input);
+  const lists = [[], []] as Array<Array<Number>>;
+
+  for (let i = 0; i < lines.length; i++) {
+    const parts = lines[i].split("   ");
+
+    lists[0][i] = Number(parts[0]);
+    lists[1][i] = Number(parts[1]);
+  }
+
+  lists[0] = lists[0].sort();
+  lists[1] = lists[1].sort();
+
+  return lists;
 }
