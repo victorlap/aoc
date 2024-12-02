6523f9bfa8159913221b3b34d3603872b9bd7a31
Victor Lap <victorlap@outlook.com>
2024-12-02 08:15:53 +0100

    Day 2 part 2

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -4,31 +4,50 @@ const input = await readInput('day-02')
 
 export const part1 = () => {
   const lines = parseLines(input).map(l => l.split(' ').map(Number))
-  let validCount = 0
 
-  for (const report of lines) {
-    const increasing = report[0] > report[1]
-    let safe = true;
+  return lines.map(checkReport).filter(Boolean).length
+}
 
-    for (let i = 0; i < report.length - 1; i++) {
-      const diff = report[i] - report[i + 1]
-      if (increasing) {
-        if (diff < 1 || diff > 3) {
-          safe = false;
-          break;
-        }
-      } else {
-        if (diff < -3 || diff > -1) {
-          safe = false;
-          break;
-        }
+export const part2 = () => {
+  const lines = parseLines(input).map(l => l.split(' ').map(Number))
+
+  return lines.map(checkReportWithDampener).filter(Boolean).length
+}
+
+const checkReport = (report: number[]) =>  {
+  const increasing = report[0] > report[1]
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const diff = report[i] - report[i + 1]
+    if (increasing) {
+      if (diff < 1 || diff > 3) {
+        return false;
       }
-    }
-
-    if (safe) {
-      validCount++;
+    } else {
+      if (diff < -3 || diff > -1) {
+        return false;
+      }
     }
   }
 
-  return validCount
+  return true;
+}
+
+const checkReportWithDampener = (report: number[]) => {
+  // Check original
+  if(checkReport(report)) {
+    return true;
+  }
+
+  // Check with one removed
+  for (let i = 0; i < report.length; i++) {
+    const copy = [...report];
+    delete copy[i];
+
+    if(checkReport(copy.filter(Number))) {
+      return true;
+    }
+  }
+
+  return false;
 }
